9431556fb19618401108fc11f7a548b8ef935ddd
gtabora <gtabora@caracolknits.com>
2020-03-31 23:23:48 -0600

    Cambio de textos a los tabs existentes

--- arcon/src/pages/Tab2.tsx
+++ arcon/src/pages/Tab2.tsx
@@ -8,16 +8,16 @@ const Tab2: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle>Goals</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
+            <IonTitle size="large">Goals</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+        <ExploreContainer name="Goals" />
       </IonContent>
     </IonPage>
   );
