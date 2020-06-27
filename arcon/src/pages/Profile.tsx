40f189a98c736de9f2cd74bf07d54deabc5d81e0
Gabriela Tábora <gabiisa03@gmail.com>
2020-06-27 14:24:47 -0600

    profiles

--- arcon/src/pages/Profile.tsx
+++ arcon/src/pages/Profile.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Profile.css';
+import React from "react";
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonItem,
+  IonIcon,
+  IonLabel,
+} from "@ionic/react";
+import ExploreContainer from "../components/ExploreContainer";
+import "./Profile.css";
+import { chevronForwardOutline } from "ionicons/icons";
 
-type ProfileProps={
-  User:{},
-  SetUser:Function
-}
+type ProfileProps = {
+  User: {};
+  SetUser: Function;
+};
 
-const Profile: React.SFC<ProfileProps> = ({User, SetUser}) => {
+const Profile: React.SFC<ProfileProps> = ({ User, SetUser }) => {
   return (
     <IonPage>
       <IonHeader collapse="condense">
         <IonToolbar>
-         
           <IonTitle size="large">Mi cuenta</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        
+        <br />
+        <IonItem>
+          <IonIcon slot="end" icon={chevronForwardOutline}></IonIcon>
+          <IonLabel>Información Personal</IonLabel>
+        </IonItem>
+        <IonItem>
+          <IonIcon slot="end" icon={chevronForwardOutline}></IonIcon>
+          <IonLabel>Perfiles</IonLabel>
+        </IonItem>  
       </IonContent>
     </IonPage>
   );
